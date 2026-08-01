740066f852ec02e6059ab15a62ec2e9658d78163
Rakvi6893 <85205869+Rakvi6893@users.noreply.github.com>
2026-08-01 18:46:21 -0400

    Update rational point computations.m

--- rational point computations.m
+++ rational point computations.m
@@ -14,3 +14,24 @@ C:=HyperellipticCurve(f);
 J:=Jacobian(C);
 #Chabauty0(J); // 3 points, all are cusps
 
+//15.96.1-15.a.1.5
+
+P<x>:=PolynomialRing(Rationals());
+
+f:=  x^3 + x^2 - 5*x + 2; 
+g:= x+1; // model taken from LMFDB
+C:=EllipticCurve(f,g);
+Rank(C); // 0 true
+#TorsionSubgroup(C); //8 points, 4 are cusps, 4 give rise to two non-CM j-invariants
+
+
+//15.96.1-15.a.2.5
+
+P<x>:=PolynomialRing(Rationals());
+
+f:=  x^3 + x^2 - 5*x + 2; 
+g:= x+1; // model taken from LMFDB
+C:=EllipticCurve(f,g);
+Rank(C); // 0 true
+#TorsionSubgroup(C); //8 points, 4 are cusps, 4 give rise to two non-CM j-invariants
+
