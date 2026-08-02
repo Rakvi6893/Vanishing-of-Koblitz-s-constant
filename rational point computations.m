4a35aaf8312f3c88568133398013d9e32d0d97f5
Rakvi6893 <85205869+Rakvi6893@users.noreply.github.com>
2026-08-01 21:08:25 -0400

    Update rational point computations.m

--- rational point computations.m
+++ rational point computations.m
@@ -8,7 +8,7 @@ J:=Jacobian(C);
 
 //14.96.2-14.e.1.1
 
-P<x>:=PolynomialRing(Rationals());
+
 f:= 7*x^5 + 63*x^4 + 91*x^3 + 28*x^2 - 7*x; // model taken from LMFDB
 C:=HyperellipticCurve(f);
 J:=Jacobian(C);
@@ -16,7 +16,7 @@ J:=Jacobian(C);
 
 //15.96.1-15.a.1.5
 
-P<x>:=PolynomialRing(Rationals());
+
 
 f:=  x^3 + x^2 - 5*x + 2; 
 g:= x+1; // model taken from LMFDB
@@ -27,7 +27,7 @@ Rank(C); // 0 true
 
 //15.96.1-15.a.2.5
 
-P<x>:=PolynomialRing(Rationals());
+
 
 f:=  x^3 + x^2 - 5*x + 2; 
 g:= x+1; // model taken from LMFDB
@@ -35,3 +35,21 @@ C:=EllipticCurve(f,g);
 Rank(C); // 0 true
 #TorsionSubgroup(C); //8 points, 4 are cusps, 4 give rise to two non-CM j-invariants
 
+//30.96.1-15.a.1.7
+
+
+f:=  x^3 + x^2 - 5*x + 2; 
+g:= x+1; // model taken from LMFDB
+C:=EllipticCurve(f,g);
+Rank(C); // 0 true
+#TorsionSubgroup(C); //8 points, 4 are cusps, 4 give rise to two non-CM j-invariants
+
+//30.96.1-15.a.2.3
+
+
+
+f:=  x^3 + x^2 - 5*x + 2; 
+g:= x+1; // model taken from LMFDB
+C:=EllipticCurve(f,g);
+Rank(C); // 0 true
+#TorsionSubgroup(C); //8 points, 4 are cusps, 4 give rise to two non-CM j-invariants
