8ca91b013727d090c44cba113fd9442983d8211d
Rakvi6893 <85205869+Rakvi6893@users.noreply.github.com>
2026-08-04 22:30:15 -0400

    Create congobstructions.m

---
+++ congobstructions.m
@@ -0,0 +1,145 @@
+D:=CremonaDatabase();
+for label in ["450d4","450d3","450d1","450d2","14400dy4","14400dy3","14400dy1","14400dy2"] do;
+label;
+E:=EllipticCurve(D, label);
+G:=FindOpenImage(E);
+level:=#BaseRing(G);
+for d in Divisors(Squarefree(level)) do;
+    if d eq 1 then continue; end if;
+    d;
+pi:=hom<GL(2,Integers(level))->GL(2,Integers(d))|[GL(2,Integers(d))!GL(2,Integers(level)).i:i in [1..#Generators(GL(2,Integers(level)))]]>;
+ Gd:=pi(G);
+ HasPrimitiveCongObstruction(Gd);
+ 
+ end for;
+ print ".........";
+ end for;
+ 
+/*output 450d4
+2
+false
+3
+false
+5
+false
+6
+false
+10
+false
+15
+true
+30
+false
+.........
+450d3
+2
+false
+3
+false
+5
+false
+6
+false
+10
+false
+15
+true
+30
+false
+.........
+450d1
+2
+false
+3
+false
+5
+false
+6
+false
+10
+false
+15
+true
+30
+false
+.........
+450d2
+2
+false
+3
+false
+5
+false
+6
+false
+10
+false
+15
+true
+30
+false
+.........
+14400dy4
+2
+false
+3
+false
+5
+false
+6
+false
+10
+false
+15
+false
+30
+true
+.........
+14400dy3
+2
+false
+3
+false
+5
+false
+6
+false
+10
+false
+15
+false
+30
+true
+.........
+14400dy1
+2
+false
+3
+false
+5
+false
+6
+false
+10
+false
+15
+false
+30
+true
+.........
+14400dy2
+2
+false
+3
+false
+5
+false
+6
+false
+10
+false
+15
+false
+30
+true
+......... */
