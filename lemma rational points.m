ca43e6955e5c6b08b9798c28572c4a609852153f
Rakvi6893 <85205869+Rakvi6893@users.noreply.github.com>
2026-09-15 20:14:56 -0400

    Update lemma rational points.m

--- lemma rational points.m
+++ lemma rational points.m
@@ -1,172 +1,67 @@
 D:=CremonaDatabase();
-H:=sub<GL(2,Integers(15))|[[11, 10, 0, 8], [11, 12, 0, 14], [14, 5, 0, 13]]>;//15.96.1-15.a.1.5
-E:=EllipticCurve(D, "50a3");//50.a2
-G:=FindOpenImage(E);
-pi:=hom<GL(2,Integers(120))->GL(2,Integers(15))|[GL(2,Integers(15))!GL(2,Integers(120)).i:i in [1..#Generators(GL(2,Integers(120)))]]>;
- G15:=pi(G);
- Index(GL(2,Integers(15)),G15); //192
- -Identity(GL(2,Integers(15))) in G15; // false
- #LowIndexSubgroups(G15,2);//4
- for d in [-3,5,-15] do;
-     d;
-     Ed:=QuadraticTwist(E,d);
-     CremonaReference(Ed);
-     G15d:=pi(FindOpenImage(Ed));
-     for T in Subgroups(H:OrderEqual:=120) do;
-         IsConjugate(GL(2,Integers(15)),T`subgroup,G15d);
-     end for;
+
+pi15:=hom<GL(2,Integers(120))->GL(2,Integers(15))|[GL(2,Integers(15))!GL(2,Integers(120)).i:i in [1..#Generators(GL(2,Integers(120)))]]>;
+pi30:=hom<GL(2,Integers(120))->GL(2,Integers(30))|[GL(2,Integers(30))!GL(2,Integers(120)).i:i in [1..#Generators(GL(2,Integers(120)))]]>;
+
+
+
+// testing coarse curves
+
+for label in ["50a1","50a2","50a3","50a4"] do;
+    E:=EllipticCurve(D, label);
+    G:=FindOpenImage(E);
+    G15:=pi15(G); assert #LowIndexSubgroups(G15,2) eq 4;
+    G30:=pi30(G); assert #LowIndexSubgroups(G30,2) eq 8;
+    S:={g:g in Generators(G15)};
+    S:=S join {-Identity(GL(2,Integers(15)))};
+    G15coarse:=sub<GL(2,Integers(15))|[g:g in S]>;
+    if HasPrimitiveCongObstruction(G15coarse) eq true then;label;end if;
+    S:={g:g in Generators(G30)}; 
+    S:=S join {-Identity(GL(2,Integers(30)))};
+    G30coarse:=sub<GL(2,Integers(30))|[g:g in S]>;
+    if HasPrimitiveCongObstruction(G30coarse)eq true then ;label;end if;
+    
 end for;
 
-//-15 is the only twist whose mod-15 image is contained in H
+for label in ["50a1","50a2","50a3","50a4"] do;
+ label;
+ E:=EllipticCurve(D, label);
 
-print "************************************************";
 
-E:=EllipticCurve(D, "50a4");//50.a4
-G:=FindOpenImage(E);
-pi:=hom<GL(2,Integers(120))->GL(2,Integers(15))|[GL(2,Integers(15))!GL(2,Integers(120)).i:i in [1..#Generators(GL(2,Integers(120)))]]>;
- G15:=pi(G);
- Index(GL(2,Integers(15)),G15); //192
- -Identity(GL(2,Integers(15))) in G15; // false
- #LowIndexSubgroups(G15,2);//4
- for d in [-3,5,-15] do;
-     d;
-     Ed:=QuadraticTwist(E,d);
+ for d in [1,-2,-3,5,-15,6,-10,30] do;
+ 
+  Ed:=QuadraticTwist(E,d);
+  Gd:=FindOpenImage(Ed);
+  Gd15:=pi15(Gd);
+  Gd30:=pi30(Gd);
+  if HasPrimitiveCongObstruction(Gd15) eq true then 
      CremonaReference(Ed);
-     G15d:=pi(FindOpenImage(Ed));
-     for T in Subgroups(H:OrderEqual:=120) do;
-         IsConjugate(GL(2,Integers(15)),T`subgroup,G15d);
-     end for;
-end for;
-
-//-15 is the only twist whose mod-15 image is contained in H
-
-
-
-H:=sub<GL(2,Integers(15))|[[2, 8, 0, 11], [4, 14, 0, 14], [13, 1, 0, 1]]>;//15.96.1-15.a.2.5
-E:=EllipticCurve(D, "50a1");//50.a3
-G:=FindOpenImage(E);
-pi:=hom<GL(2,Integers(120))->GL(2,Integers(15))|[GL(2,Integers(15))!GL(2,Integers(120)).i:i in [1..#Generators(GL(2,Integers(120)))]]>;
- G15:=pi(G);
- Index(GL(2,Integers(15)),G15); //192
- -Identity(GL(2,Integers(15))) in G15; // false
- #LowIndexSubgroups(G15,2);//4
- for d in [-3,5,-15] do;
-     d;
-     Ed:=QuadraticTwist(E,d);
+  end if;
+ 
+  if HasPrimitiveCongObstruction(Gd30) eq true then
      CremonaReference(Ed);
-     G15d:=pi(FindOpenImage(Ed));
-     for T in Subgroups(H:OrderEqual:=120) do;
-         IsConjugate(GL(2,Integers(15)),T`subgroup,G15d);
-     end for;
-end for;
+  end if;
+ 
+ 
+  end for;
+  print ".........";
+ end for;
 
-//-15 is the only twist whose mod-15 image is contained in H
+ /* 50a1
+450d3
+14400dy3
+.........
+50a2
+450d4
+14400dy4
+.........
+50a3
+450d1
+14400dy1
+.........
+50a4
+450d2
+14400dy2
+.........*/
+ 
 
-print "************************************************";
-
-E:=EllipticCurve(D, "50a2");//50.a1
-G:=FindOpenImage(E);
-pi:=hom<GL(2,Integers(120))->GL(2,Integers(15))|[GL(2,Integers(15))!GL(2,Integers(120)).i:i in [1..#Generators(GL(2,Integers(120)))]]>;
- G15:=pi(G);
- Index(GL(2,Integers(15)),G15); //192
- -Identity(GL(2,Integers(15))) in G15; // false
- #LowIndexSubgroups(G15,2);//4
- for d in [-3,5,-15] do;
-     d;
-     Ed:=QuadraticTwist(E,d);
-     CremonaReference(Ed);
-     G15d:=pi(FindOpenImage(Ed));
-     for T in Subgroups(H:OrderEqual:=120) do;
-         IsConjugate(GL(2,Integers(15)),T`subgroup,G15d);
-     end for;
-end for;
-
-//-15 is the only twist whose mod-15 image is contained in H
-
-
-H:=sub<GL(2,Integers(30))|[[19, 0, 12, 17], [19, 15, 27, 26], [29, 20, 18, 7], [29, 20, 27, 29]]>;//30.96.1-15.a.2.3
-E:=EllipticCurve(D, "50a1");//50.a3
-G:=FindOpenImage(E);
-pi:=hom<GL(2,Integers(120))->GL(2,Integers(30))|[GL(2,Integers(30))!GL(2,Integers(120)).i:i in [1..#Generators(GL(2,Integers(120)))]]>;
- G30:=pi(G);
- Index(GL(2,Integers(30)),G30); //192
- -Identity(GL(2,Integers(30))) in G30; // false
- #LowIndexSubgroups(G30,2);//8
- for d in [-2,-3,5,-15,6,-10,30] do;
-     d;
-     Ed:=QuadraticTwist(E,d);
-     CremonaReference(Ed);
-     G30d:=pi(FindOpenImage(Ed));
-     for T in Subgroups(H:OrderEqual:=720) do;
-         IsConjugate(GL(2,Integers(30)),T`subgroup,G30d);
-     end for;
-end for;
-
-//30 is the only twist whose mod-15 image is contained in H
-
-print "************************************************";
-
-E:=EllipticCurve(D, "50a2");//50.a1
-G:=FindOpenImage(E);
-pi:=hom<GL(2,Integers(120))->GL(2,Integers(30))|[GL(2,Integers(30))!GL(2,Integers(120)).i:i in [1..#Generators(GL(2,Integers(120)))]]>;
- G30:=pi(G);
- Index(GL(2,Integers(30)),G30); //192
- -Identity(GL(2,Integers(30))) in G30; // false
- #LowIndexSubgroups(G30,2);//8
- for d in [-2,-3,5,-15,6,-10,30] do;
-     d;
-     Ed:=QuadraticTwist(E,d);
-     CremonaReference(Ed);
-     G30d:=pi(FindOpenImage(Ed));
-     for T in Subgroups(H:OrderEqual:=720) do;
-         IsConjugate(GL(2,Integers(30)),T`subgroup,G30d);
-     end for;
-end for;
-
-//30 is the only twist whose mod-15 image is contained in H
-
-print "************************************************";
-
-D:=CremonaDatabase();
-H:=sub<GL(2,Integers(30))|[[1, 5, 12, 11], [4, 5, 15, 29], [8, 15, 9, 16], [26, 25, 27, 19]]>;//30.96.1-15.a.1.7
-E:=EllipticCurve(D, "50a3");//50.a2
-G:=FindOpenImage(E);
-pi:=hom<GL(2,Integers(120))->GL(2,Integers(30))|[GL(2,Integers(30))!GL(2,Integers(120)).i:i in [1..#Generators(GL(2,Integers(120)))]]>;
- G30:=pi(G);
- Index(GL(2,Integers(30)),G30); //192
- -Identity(GL(2,Integers(30))) in G30; // false
- #LowIndexSubgroups(G30,2);//8
- for d in [-2,-3,5,-15,6,-10,30] do;
-     d;
-     Ed:=QuadraticTwist(E,d);
-     CremonaReference(Ed);
-     G30d:=pi(FindOpenImage(Ed));
-     for T in Subgroups(H:OrderEqual:=720) do;
-         IsConjugate(GL(2,Integers(30)),T`subgroup,G30d);
-     end for;
-end for;
-
-//30 is the only twist whose mod-15 image is contained in H
-
-print "************************************************";
-
-E:=EllipticCurve(D, "50a4");//50.a4
-G:=FindOpenImage(E);
-pi:=hom<GL(2,Integers(120))->GL(2,Integers(30))|[GL(2,Integers(30))!GL(2,Integers(120)).i:i in [1..#Generators(GL(2,Integers(120)))]]>;
- G30:=pi(G);
- Index(GL(2,Integers(30)),G30); //192
- -Identity(GL(2,Integers(30))) in G30; // false
- #LowIndexSubgroups(G30,2);//8
- for d in [-2,-3,5,-15,6,-10,30] do;
-     d;
-     Ed:=QuadraticTwist(E,d);
-     CremonaReference(Ed);
-     G30d:=pi(FindOpenImage(Ed));
-     for T in Subgroups(H:OrderEqual:=720) do;
-         IsConjugate(GL(2,Integers(30)),T`subgroup,G30d);
-     end for;
-end for;
-
-//30 is the only twist whose mod-15 image is contained in H
-
-print "************************************************";
