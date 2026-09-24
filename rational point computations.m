d2b64676b7bbafbe05f9f0f91d69df6d6b2370b3
Jacob Mayle <61399623+maylejacobj@users.noreply.github.com>
2026-09-24 11:00:18 -0400

    Update rational point computations.m

--- rational point computations.m
+++ rational point computations.m
@@ -1,64 +1,27 @@
-//14.96.2-14.e.2.1
-
 P<x>:=PolynomialRing(Rationals());
-f:= - 7*x^5 + 63*x^4 - 91*x^3 + 28*x^2 + 7*x; // model taken from LMFDB
-C:=HyperellipticCurve(f);
-J:=Jacobian(C);
-#Chabauty0(J); // 3 points, all are cusps
-
-//14.96.2-14.e.1.1
-
-
-f:= 7*x^5 + 63*x^4 + 91*x^3 + 28*x^2 - 7*x; // model taken from LMFDB
-C:=HyperellipticCurve(f);
-J:=Jacobian(C);
-#Chabauty0(J); // 3 points, all are cusps
-
-//15.96.1-15.a.1.5
-
-
-
-f:=  x^3 + x^2 - 5*x + 2; 
-g:= x+1; // model taken from LMFDB
-C:=EllipticCurve(f,g);
-Rank(C); // 0 true
-#TorsionSubgroup(C); //8 points, 4 are cusps, 4 give rise to two non-CM j-invariants
-
-
-//15.96.1-15.a.2.5
-
-
-
-f:=  x^3 + x^2 - 5*x + 2; 
-g:= x+1; // model taken from LMFDB
-C:=EllipticCurve(f,g);
-Rank(C); // 0 true
-#TorsionSubgroup(C); //8 points, 4 are cusps, 4 give rise to two non-CM j-invariants
-
-//30.96.1-15.a.1.7
-
-
-f:=  x^3 + x^2 - 5*x + 2; 
-g:= x+1; // model taken from LMFDB
-C:=EllipticCurve(f,g);
-Rank(C); // 0 true
-#TorsionSubgroup(C); //8 points, 4 are cusps, 4 give rise to two non-CM j-invariants
-
-//30.96.1-15.a.2.3
-
-
-
-f:=  x^3 + x^2 - 5*x + 2; 
-g:= x+1; // model taken from LMFDB
-C:=EllipticCurve(f,g);
-Rank(C); // 0 true
-#TorsionSubgroup(C); //8 points, 4 are cusps, 4 give rise to two non-CM j-invariants
 
 //10.24.1.b.1 and 10.24.1.b.2 (both have isomorphic models)
-
-f:=  x^3 - x^2 - 1033*x - 12438; 
+f:=  x^3 - x^2 - 1033*x - 12438; // model taken from LMFDB
 C:=EllipticCurve(f);
 Rank(C); // 0 true
 #TorsionSubgroup(C);// 2 both are cusps
 
+//14.48.2.e.1
+f:= 7*x^5 + 63*x^4 + 91*x^3 + 28*x^2 - 7*x; // model taken from LMFDB
+C:=HyperellipticCurve(f);
+J:=Jacobian(C);
+RankBounds(J); // Rank 0
+#Chabauty0(J); // 3 points, all are cusps
 
+//14.48.2.e.2
+f:= - 7*x^5 + 63*x^4 - 91*x^3 + 28*x^2 + 7*x; // model taken from LMFDB
+C:=HyperellipticCurve(f);
+J:=Jacobian(C);
+RankBounds(J); // Rank 0
+#Chabauty0(J); // 3 points, all are cusps
+
+//15.48.1.a.1 and 15.48.1.a.2 (which have isomorphic models)
+f:=  x^3 + x^2 - 5*x + 2; g:= x+1; // model taken from LMFDB
+C:=EllipticCurve(f,g);
+Rank(C); // 0 true
+#TorsionSubgroup(C); //8 points, 4 are cusps, 4 give rise to two non-CM j-invariants
