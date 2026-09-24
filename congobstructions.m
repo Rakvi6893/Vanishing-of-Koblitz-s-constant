717b745c54e1ef4232cc3e95333d4938571463fb
Rakvi6893 <85205869+Rakvi6893@users.noreply.github.com>
2026-09-24 12:05:13 -0400

    Update congobstructions.m

--- congobstructions.m
+++ congobstructions.m
@@ -1,5 +1,20 @@
 ChangeDirectory("/Users/Jacob/OpenImage-master"); // Change this line as needed.
 load "main/FindOpenImage.m";
+load "Common.m";
+
+function HasPrimitiveCongObstruction(G)
+    m:=#BaseRing(G);
+    S:=[];
+    for d in Divisors(Squarefree(m)) do;
+        if d eq 1 or d eq m then continue; end if;
+        pi:=hom<GL(2,Integers(m))->GL(2,Integers(d))|[GL(2,Integers(d))!GL(2,Integers(m)).i:i in [1..#Generators(GL(2,Integers(m)))]]>;
+        Gd:=pi(G); 
+        S:= S cat [F1(Gd)];
+    end for;
+    if F1(G) eq 1 and 1 notin S then return true;
+    else return false; end if;
+end function;
+
 
 D:=CremonaDatabase();
 for label in ["450d4","450d3","450d1","450d2","14400dy4","14400dy3","14400dy1","14400dy2"] do;
